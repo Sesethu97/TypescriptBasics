6d11417f744bbd3cd75a75f1945d4f64bf674f86
sluzipo021 <sluzipo021@student.wethinkcode.co.za>
2022-11-09 22:33:39 +0200

    variables

---
+++ variables/one.ts
@@ -0,0 +1,17 @@
+var str = '1' 
+var str2:number = <number> <any> str    
+console.log(typeof(str2))
+
+var num1 = 1
+class numbers{
+    num2 = 2;
+    static num3 = 4;
+    
+    storenum() : void {
+        var num4 : 5;
+    }
+}
+console.log("Global num: "+num1)  
+console.log(numbers.num3)   //static variable  
+var obj = new numbers(); 
+console.log("Global num: "+obj.num2) 
